0bc33a3ae1c9756845b0aa85dacabf09a13d864a
kslzjw <zhangjinweio@163.com>
2022-10-17 00:39:52 +0800

    添加nacos组件

--- coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/zjw/coupon/calculation/CalculationApplication.java
+++ coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/zjw/coupon/calculation/CalculationApplication.java
@@ -2,9 +2,11 @@ package com.zjw.coupon.calculation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan({"com.zjw"})
 public class CalculationApplication {
     public static void main(String[] args) {
